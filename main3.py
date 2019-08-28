4453de26c1f7b3b395f8bc3665fa2ce379b066ee
Murata <tamurata@iu.edu>
2019-08-28 19:41:57 -0400

    new change

--- main3.py
+++ main3.py
@@ -10,12 +10,12 @@ arcade.set_background_color(open_color.white)
 # Start the render process. This must be done before any drawing commands.
 arcade.start_render()
 
-face_x,face_y = (0,0)
+face_x,face_y = (400,300)
 smile_x,smile_y = (face_x + 0,face_y - 0)
-eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-eye2_x,eye2_y = (face_x + 0,face_y + 0)
-catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+eye1_x,eye1_y = (face_x - 27,face_y + 30) 
+eye2_x,eye2_y = (face_x + 27,face_y + 30)
+catch1_x,catch1_y = (face_x - 24,face_y + 35) 
+catch2_x,catch2_y = (face_x + 30,face_y + 35) 
 
 
 # Draw the smiley face:
