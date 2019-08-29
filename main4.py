baa973b059be982532ec4be7dec0451c1aa1f64d
tamurata <tamurata@iu.edu>
2019-08-28 20:50:05 -0400

    Final edit

--- main4.py
+++ main4.py
@@ -18,10 +18,10 @@ for x in range(100,800,150):
         for y in range(100,600,150):
                 face_x,face_y = (x,y)
                 smile_x,smile_y = (face_x + 0,face_y - 0)
-                eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-                eye2_x,eye2_y = (face_x + 0,face_y + 0)
-                catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-                catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+                eye1_x,eye1_y = (face_x - 30,face_y + 20) 
+                eye2_x,eye2_y = (face_x + 30,face_y + 20)
+                catch1_x,catch1_y = (face_x - 25,face_y + 30) 
+                catch2_x,catch2_y = (face_x + 35,face_y + 30) 
 
 
                 # Draw the smiley face:
--- main5.py
+++ main5.py
@@ -4,11 +4,11 @@ import utils, open_color, arcade
 
 utils.check_version((3,7))
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Smiley Face Example"
+SCREEN_WIDTH = 800   
+SCREEN_HEIGHT = 600   #Line 7,8 are deciding how big the screen will be
+SCREEN_TITLE = "Smiley Face Example"  #The title of the page
 
-class Faces(arcade.Window):
+class Faces(arcade.Window):  
     """ Our custom Window Class"""
 
     def __init__(self):
@@ -17,10 +17,10 @@ class Faces(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         # Show the mouse cursor
-        self.set_mouse_visible(True)
+        self.set_mouse_visible(True) #probably self tag means where the mouse cursor locate
 
         self.x = SCREEN_WIDTH / 2
-        self.y = SCREEN_HEIGHT / 2
+        self.y = SCREEN_HEIGHT / 2  #this decides where the mouse cursor starts located
 
         arcade.set_background_color(open_color.white)
 
@@ -32,7 +32,7 @@ class Faces(arcade.Window):
         eye1_x,eye1_y = (face_x - 30,face_y + 20) 
         eye2_x,eye2_y = (face_x + 30,face_y + 20)
         catch1_x,catch1_y = (face_x - 25,face_y + 25) 
-        catch2_x,catch2_y = (face_x + 35,face_y + 25) 
+        catch2_x,catch2_y = (face_x + 35,face_y + 25)  #decides where the parts are located
 
         arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3)
         arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
@@ -40,13 +40,13 @@ class Faces(arcade.Window):
         arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
         arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2)
         arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2)
-        arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4)
+        arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4)  #the size of each parts
 
 
     def on_mouse_motion(self, x, y, dx, dy):
         """ Handle Mouse Motion """
         self.x = x
-        self.y = y
+        self.y = y #This activates that the smily face follow where the mouse cursor goes
 
 
 
